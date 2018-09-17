4d170a7e236d1077f64ec5556f395df3255f7afb
zhuangpeifen <zhuangpeifen@rrjc.com>
2018-09-17 18:02:06 +0800

    no message

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
+router.get('/test', function(req, res, next) {
+  res.send('test');
+});
 
 module.exports = router;
